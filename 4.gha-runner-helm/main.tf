4f638829a31e26539468ee206b6d4b34445bd540
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-07-18 14:16:10 +0800

    :beers: eploy gha runner controller helm charts :beers:

--- 4.gha-runner-helm/main.tf
+++ 4.gha-runner-helm/main.tf
@@ -7,26 +7,26 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host                   = local.cluster_endpoint
+  host                   = local.eks_cluster_endpoint
   cluster_ca_certificate = base64decode(local.eks_cluster_ca_certificate)
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    args = ["eks", "get-token", "--cluster-name", local.eks_cluster_name]
   }
 }
 
 provider "helm" {
   kubernetes = {
-    host                   = local.cluster_endpoint
+    host                   = local.eks_cluster_ca_certificate
     cluster_ca_certificate = base64decode(local.eks_cluster_ca_certificate)
 
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      args        = ["eks", "get-token", "--cluster-name", local.eks_cluster_name]
     }
   }
 }
