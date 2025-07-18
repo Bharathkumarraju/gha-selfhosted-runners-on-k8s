f2754674eddc49a72485ddb50c439b6acecbc76d
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-07-18 10:29:11 +0800

    :safety_vest: deploy aws vpc in ap-south1 region :safety_vest:

--- 2.aws-vpc/data.tf
+++ 2.aws-vpc/data.tf
@@ -1,7 +1,5 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
-data "aws_availability_zones" "all" {
-  state = "available"
-}
+data "aws_availability_zones" "available" {}
 
--- 3.aws-eks/data.tf
+++ 3.aws-eks/data.tf
@@ -1,9 +1,7 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
-data "aws_availability_zones" "all" {
-  state = "available"
-}
+data "aws_availability_zones" "available" {}
 
 
 data "terraform_remote_state" "aws-vpc" {
